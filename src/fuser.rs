9120ff7e7f7c91aea19bcce14bfab859a26baa95
Tanveer Raza <tanveerraza789@gmail.com>
2023-04-20 14:05:56 +0530

    feat: add ability to check is mic is opened or not

--- src/fuser.rs
+++ src/fuser.rs
@@ -1,6 +1,32 @@
 //! A basic functionality replacing the use of `fuser` command on linux
 use std::fs;
 
+/// Simply returns true is a file is accessed any process.
+pub(crate) fn fusers_is_open(file_path: &str) -> bool {
+    if let Ok(entries) = fs::read_dir("/proc") {
+        for entry in entries.flatten() {
+            let meta = entry.metadata();
+            if meta.is_ok() && meta.unwrap().is_dir() {
+                if let Ok(ref pid) = entry.file_name().into_string().unwrap().parse::<i32>() {
+                    if let Ok(fds) = fs::read_dir(format!("/proc/{}/fd", pid)) {
+                        for fd in fds.flatten() {
+                            if let Ok(ref opened_file) = fs::read_link(fd.path()) {
+                                if let Some(open_file_name) = opened_file.to_str() {
+                                    if open_file_name == file_path {
+                                        // Instant return if we find file is open be it any process
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    false
+}
+
 /// Get the PIDs of all the process that have openend the file.
 pub(crate) fn fusers(file_path: &str) -> Vec<i32> {
     let mut pids = Vec::new();
