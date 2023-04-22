7a5f6ead692273cb17db8c4e9898860490cea6f7
0xlax <harshu8310@gmail.com>
2023-04-22 16:34:59 +0530

    add matches! macro for faster result

--- src/fuser.rs
+++ src/fuser.rs
@@ -2,20 +2,19 @@
 use std::fs;
 
 /// Simply returns true is a file is accessed any process.
-pub(crate) fn fusers_is_open(file_path: &str) -> bool {
+
+
+pub(crate)fn fusers_is_open(file_path: &str) -> bool {
     if let Ok(entries) = fs::read_dir("/proc") {
         for entry in entries.flatten() {
             let meta = entry.metadata();
             if meta.is_ok() && meta.unwrap().is_dir() {
-                if let Ok(ref pid) = entry.file_name().into_string().unwrap().parse::<i32>() {
+                if let Ok(pid) = entry.file_name().into_string() {
                     if let Ok(fds) = fs::read_dir(format!("/proc/{}/fd", pid)) {
                         for fd in fds.flatten() {
                             if let Ok(ref opened_file) = fs::read_link(fd.path()) {
-                                if let Some(open_file_name) = opened_file.to_str() {
-                                    if open_file_name == file_path {
-                                        // Instant return if we find file is open be it any process
-                                        return true;
-                                    }
+                                if matches!(opened_file.to_str(), Some(open_file_name) if open_file_name == file_path) {
+                                    return true;
                                 }
                             }
                         }
@@ -26,6 +25,8 @@ pub(crate) fn fusers_is_open(file_path: &str) -> bool {
     }
     false
 }
+
+
 
 /// Get the PIDs of all the process that have openend the file.
 pub(crate) fn fusers(file_path: &str) -> Vec<i32> {
